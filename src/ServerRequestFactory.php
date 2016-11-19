05527096780858a0f93e8e1c94e9e22011f1fad9
Woody Gilk <woody.gilk@gmail.com>
2016-11-19 10:09:03 -0600

    Update implementation from interfaces
    Stream and server factory have been updated in v0.2.0.

--- src/ServerRequestFactory.php
+++ src/ServerRequestFactory.php
@@ -7,17 +7,33 @@ use Interop\Http\Factory\ServerRequestFromGlobalsFactoryInterface;
 use Zend\Diactoros\ServerRequest;
 use Zend\Diactoros\ServerRequestFactory as Factory;
 
-class ServerRequestFactory implements
-    ServerRequestFactoryInterface,
-    ServerRequestFromGlobalsFactoryInterface
+class ServerRequestFactory implements ServerRequestFactoryInterface
 {
-    public function createServerRequest($method, $uri)
+    public function createServerRequest(array $server, $method = null, $uri = null)
     {
-        return new ServerRequest([], [], $uri, $method);
-    }
+        $server  = Factory::normalizeServer($server ?: $_SERVER);
+        $files   = Factory::normalizeFiles($files ?: $_FILES);
+        $headers = Factory::marshalHeaders($server);
 
-    public function createServerRequestFromGlobals()
-    {
-        return Factory::fromGlobals();
+        if (empty($method)) {
+            $method = Factory::get('REQUEST_METHOD', $server, 'GET');
+        }
+
+        if (empty($uri)) {
+            $uri = Factory::marshalUriFromServer($server, $headers);
+        }
+
+        return new ServerRequest(
+            $server,
+            $files,
+            $uri,
+            $method,
+            'php://input',
+            $headers,
+            $_COOKIE,
+            $_GET,
+            $_POST,
+            static::marshalProtocolVersion($server)
+        );
     }
 }
